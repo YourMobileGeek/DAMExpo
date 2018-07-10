b360377ee4176ff3179c9c4e222f92dbed68c60c
cluzier <connerluzier@outlook.com>
2018-07-10 00:54:07 -0400

    final changes to particlesjs background music notes.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles.min.js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+  console.log('callback - particles.js config loaded');
+});
